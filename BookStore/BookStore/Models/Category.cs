2d9e2bc4afbb9c1f608f614e66ee666b71b06b3e
Matthew Bird <98002021+MatthewBird625@users.noreply.github.com>
2022-11-25 12:59:53 +1100

    currentDotNetWork
    backup of my progress so far on .Net course

---
+++ BookStore/BookStore/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Category
+    {
+
+        //data annotation that creates a primary key
+        [Key]
+        public int Id { get; set; }
+        //makes following member variable compulsory(required)
+        [Required]
+        public string Name { get; set; }
+  
+        public int DisplayOrder { get; set; }
+        public DateTime? CreatedDateTime { get; set; } = DateTime.Now;  
+    }
+}
